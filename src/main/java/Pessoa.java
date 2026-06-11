df6298cfbb19c1910b4a66a2c420c6bcc7f9179b
michelle2526 <90350891+michelle2526@users.noreply.github.com>
2026-06-11 09:47:12 -0300

    .
    DESVENDADO O BUG DO SITEMA DE CALCULADORA

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -27,7 +27,7 @@ public class Pessoa extends PessoaBase implements CalculadoraIMC {
         return ativo;
     }
 
-    // Setter com validação
+    // Setters com validação
     public void setPeso(double peso) {
 
         if (peso <= 0) {
@@ -37,11 +37,20 @@ public class Pessoa extends PessoaBase implements CalculadoraIMC {
         this.peso = peso;
     }
 
-    // Implementação do cálculo do IMC
+    public void setAltura(double altura) {
+
+        if (altura <= 0) {
+            throw new IllegalArgumentException("Altura deve ser positiva!");
+        }
+
+        this.altura = altura;
+    }
+
+    // Implementação do cálculo do IMC usando recursão
     @Override
     public double calcularIMC(double peso, double altura) {
 
-        return peso / (altura * altura);
+        return peso / CalculadoraRecursiva.potencia(altura, 2);
     }
 
     // Implementação da classificação
@@ -50,25 +59,15 @@ public class Pessoa extends PessoaBase implements CalculadoraIMC {
 
         if (imc < 18.5) {
             return "Abaixo do peso";
-        }
-
-        else if (imc < 25.0) {
+        } else if (imc < 25.0) {
             return "Peso normal";
-        }
-
-        else if (imc < 30.0) {
+        } else if (imc < 30.0) {
             return "Sobrepeso";
-        }
-
-        else if (imc < 35.0) {
+        } else if (imc < 35.0) {
             return "Obesidade grau I";
-        }
-
-        else if (imc < 40.0) {
+        } else if (imc < 40.0) {
             return "Obesidade grau II";
-        }
-
-        else {
+        } else {
             return "Obesidade grau III";
         }
     }
@@ -78,8 +77,8 @@ public class Pessoa extends PessoaBase implements CalculadoraIMC {
     public String exibirPerfil() {
 
         return "Nome: " + nome +
-               " | Idade: " + idade +
-               " | Peso: " + peso +
-               "kg | Altura: " + altura + "m";
+                " | Idade: " + idade +
+                " | Peso: " + peso +
+                "kg | Altura: " + altura + "m";
     }
 }
